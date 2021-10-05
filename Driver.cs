82a8e41e3451690fd3f1a909102a3113f9440b1d
suleman-afk <bcsf18m002@pucit.edu.pk>
2021-10-05 22:02:42 +0500

    added sum 2

--- Driver.cs
+++ Driver.cs
@@ -16,13 +16,23 @@ namespace firstDayInEAD
             int num2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Sum of {0} and {1} is {2}", num1, num2, num1 + num2);
         }
-        
+
+        public static int sum2(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+      
+
         private static void Main()
         {
             sum1();
 
 
-            
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum of {0} and {1} is {2}", num1, num2, sum2(num1, num2));
 
 
 
